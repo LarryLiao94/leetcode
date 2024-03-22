766a4737a9df2d88b2a657b2522477ad39cefcff
Larry Liao <91893259+LarryLiao94@users.noreply.github.com>
2024-03-22 11:34:09 -0400

    Time: 93 ms (98.79%) | Memory: 64.4 MB (97.62%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    if (!head || !head.next)
+        return true;
+
+    let slow = head;
+    let fast = head;
+    let prev = null;
+
+    // Find the middle of the linked list and reverse the first half
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        let temp = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = temp;
+    }
+
+    if (fast)
+        slow = slow.next;
+
+    while (prev && slow) {
+        if (prev.val !== slow.val)
+            return false;
+        prev = prev.next;
+        slow = slow.next;
+    }
+
+    return true;
+};
