583e51502d85802dfc6eb779f1a99a76a956989a
Larry Liao <91893259+LarryLiao94@users.noreply.github.com>
2024-03-25 09:11:37 -0400

    Time: 82 ms (72.10%) | Memory: 57.2 MB (92.95%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+     const result = [];
+        const n = nums.length;
+        for (let i = 0; i < n; i++) {
+            const num = Math.abs(nums[i]);
+            const idx = num - 1;
+            if (nums[idx] < 0)
+                result.push(num);
+            nums[idx] *= -1;
+        }
+        return result;
+};
